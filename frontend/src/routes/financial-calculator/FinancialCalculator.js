06ab1a1cc3f67a4ae0ab9758d21aaa41ef219813
Jessica Dawson <jessica@jessica-thinkpad.ezlink.ca>
2021-04-05 23:00:37 -0400

    financial calculator completed

--- frontend/src/routes/financial-calculator/FinancialCalculator.js
+++ frontend/src/routes/financial-calculator/FinancialCalculator.js
@@ -3,15 +3,30 @@ import './FinancialCalculator.css';
 
 const FinancialCalculator = () => {
 	const [formData, setFormData] = useState({
-		currentInterest: null,
-		outstandingLoanCost: null
+		nominalInterest: null,
+		outstandingLoan: null,
+		loanPayment: null
 	});
 
-	const [displayedPayback, setDisplayedPayback] = useState(null);
+	const [cannotPayBack, setCannotPayBack] = useState(null);
+
+	const [displayedPayback, setDisplayedPayback] = useState({
+		years: null,
+		months: null
+	});
 
 	const calculatePayback = (event) => {
 		event.preventDefault();
-		setDisplayedPayback(formData.currentInterest + formData.outstandingLoanCost); // TODO: Change this.
+		var effectiveInterest = (Math.pow((1 + ((formData.nominalInterest)/100)/365), 30.42) - 1);
+		if ((formData.loanPayment - formData.outstandingLoan*effectiveInterest) <= 0) {
+			setDisplayedPayback({...displayedPayback, years: null, months: null});
+			setCannotPayBack(true);
+		} else {
+			setCannotPayBack(null);
+			var totalMonths = Math.ceil( (Math.log(formData.loanPayment) - Math.log(formData.loanPayment - formData.outstandingLoan * effectiveInterest)) 
+			/ Math.log(1 + effectiveInterest));
+			setDisplayedPayback({...displayedPayback, years: (Math.floor(totalMonths / 12)), months: (totalMonths % 12)});
+		}
 	}
 
 	return (
@@ -20,32 +35,47 @@ const FinancialCalculator = () => {
 			<form onSubmit={calculatePayback}>
 				<div className='FormInputsWrapper'>
 					<label className='InputWrapper'>
-						Current Interest
+						Outstanding Loan
 						<input
 							type='number'
+							step='0.01'
 							className='FormInput'
-							value={formData.currentInterest}
+							value={formData.outstandingLoan}
 							onChange={event => setFormData({
 								...formData,
-								currentInterest: event.target.value
+								outstandingLoan: parseFloat(event.target.value)
 							})} />
 					</label>
 					<label className='InputWrapper'>
-						Outstanding Loan Cost
+						Current Interest Rate
 						<input
 							type='number'
+							step='0.01'
 							className='FormInput'
-							value={formData.outstandingLoanCost}
+							value={formData.nominalInterest}
 							onChange={event => setFormData({
 								...formData,
-								outstandingLoanCost: event.target.value
+								nominalInterest: parseFloat(event.target.value)
+							})} />
+					</label>
+					<label className='InputWrapper'>
+						Monthly Loan Payment
+						<input
+							type='number'
+							step='0.01'
+							className='FormInput'
+							value={formData.loanPayment}
+							onChange={event => setFormData({
+								...formData,
+								loanPayment: parseFloat(event.target.value)
 							})} />
 					</label>
 					<input className='SubmitButton' type='submit' value='Submit' />
 				</div>
 			</form>
 			<div>
-				{ displayedPayback && <><h3>The calculated payback is:</h3><p>{displayedPayback}</p></> }
+				{ displayedPayback.years && <><h3>The calculated payback time is:</h3><p>{displayedPayback.years} yrs {displayedPayback.months} mths</p></> }
+				{ cannotPayBack && <><h3>The loan can not be paid back with this payment amount</h3></> }
 			</div>
 		</div>
 	);
