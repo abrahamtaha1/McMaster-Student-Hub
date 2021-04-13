4a74e100a8d95d60bbba30df780a1273bb2d7244
Donisius <donisius.wigie@gmail.com>
2021-04-12 21:41:19 -0400

    Add table

--- frontend/src/routes/reviews/Reviews.js
+++ frontend/src/routes/reviews/Reviews.js
@@ -1,10 +1,18 @@
 import React, { useState } from 'react';
+import { DataGrid } from '@material-ui/data-grid';
 import './Reviews.css';
 
+const columns = [
+    { field: 'timeStamp', headerName: 'Time Stamp', width: 200 },
+    { field: 'propertyAddress', headerName: 'Property Address', width: 200 },
+    { field: 'monthyRent', headerName: 'Monthy Rent', width: 200 },
+    { field: 'landlordScore', headerName: 'Landlord Score', width: 200 },
+    { field: 'numberOfRoomates', headerName: 'Number Of Roomates', width: 200 },
+    { field: 'overallPropertyRating', headerName: 'Overall Property Rating', width: 200 },
+];
+
 const Reviews = () => {
-    const [results, setResults] = useState({
-        data: []
-    });
+    const [tableData, setTableData] = useState([])
 
     const [filterData, setFilterData] = useState({
 		maxPrice: null,
@@ -15,17 +23,27 @@ const Reviews = () => {
 
     const makeRequest = async () => {
         const res = await (await fetch('http://localhost:3001/reviews')).json();
-        setResults({
-            ...results,
-            data: [...results.data, res.data]
-        })
+        const resValues = res.values;
+        resValues.shift()
+        const rows = resValues
+        setTableData(rows.map((row, index) => ({
+            id: index,
+            timeStamp: row[0],
+            propertyAddress: row[1],
+            montlyRent: row[2],
+            landlordScore: row[3],
+            numberOfRoomates: row[4],
+            overallPropertyRating: row[5]
+        })))
     }
 
 	return (
 		<div>
 			<h1>Property Reviews</h1>
             <button onClick={() => { makeRequest() }}>Make a test request</button>
-            { results?.data.map((data, index) => <h1 key={index}>{ data }</h1>) }
+            <div style={{ height: 400, width: '100%' }}>
+                <DataGrid rows={tableData} columns={columns} pageSize={5} checkboxSelection />
+            </div>
 		</div>
 	);
 }
